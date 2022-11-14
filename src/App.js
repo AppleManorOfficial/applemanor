9d624b05a0426acf119b2dfc87cb87a93c5b6967
apple manor use <greg65425@yahoo.com.tw>
2022-11-15 01:19:43 +0800

    luis test 404 function

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import ProductListHandMade from './pages/productList/productList-HandMade';
 import ProductListLiving from './pages/productList/productList-Living';
 import ProductListHerbal from './pages/productList/productList-Herbal';
 import ProductListGift from './pages/productList/productList-Gift';
+import Page404 from './pages/404/Page404';
 import ScrollToTop from './component/scrollToTop/ScrollToTop';
 import './App.scss';
 import ContatctUs from './pages/ContactUs/ContatctUs';
@@ -19,25 +20,30 @@ function App() {
       <Header></Header>
       <ScrollToTop />
       <Routes>
-        <Route path="/applemanor" element={<Homepage></Homepage>}></Route>
-        <Route path="/aboutus" element={<AboutUs></AboutUs>}></Route>
+        <Route path="*" element={<Page404></Page404>}></Route>
+        <Route exact path="/applemanor" element={<Homepage></Homepage>}></Route>
+        <Route exact path="/aboutus" element={<AboutUs></AboutUs>}></Route>
         <Route
+          exact
           path="/handmade"
           element={<ProductListHandMade></ProductListHandMade>}
         ></Route>
         <Route
+          exact
           path="/herbal"
           element={<ProductListHerbal></ProductListHerbal>}
         ></Route>
         <Route
+          exact
           path="/living"
           element={<ProductListLiving></ProductListLiving>}
         ></Route>
         <Route
+          exact
           path="/giftbox"
           element={<ProductListGift></ProductListGift>}
         ></Route>
-        <Route path="/contactus" element={<ContatctUs />}></Route>
+        <Route exact path="/contactus" element={<ContatctUs />}></Route>
       </Routes>
 
       <Footer></Footer>
---
+++ src/pages/404/Page404.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Page404.scss';
+
+function Page404() {
+  return (
+    <div className="Page404">
+      <div className="mark">404</div>
+      <p className="des">抱歉沒有此網頁內容～～</p>
+    </div>
+  );
+}
+
+export default Page404;
